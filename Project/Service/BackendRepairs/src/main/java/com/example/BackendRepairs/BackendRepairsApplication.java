d4d142390d408d0834e49211a8412d5f63f6205a
VUONGVANHAU_SE_2019 <vanhauspkt1997@gmail.com>
2019-03-26 23:05:45 +0700

    Create Project

---
+++ Project/Service/BackendRepairs/src/main/java/com/example/BackendRepairs/BackendRepairsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendRepairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRepairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRepairsApplication.class, args);
+	}
+
+}
